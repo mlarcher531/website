e1d26e6e3a1490ab5a1e80ef2f8f0051a9e06f2f
mlarcher531 <mlarcher531@gmail.com>
2020-12-28 13:16:35 -0600

    remove bootstrap

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Card, Button } from 'react-bootstrap'
+
+
 
 
 function App() {
   return (
     <div className="App">
-      <Card>
-        <h1>hello world</h1>
-        <Button>hi</Button>
-      </Card>
+      <div className="zoo">
+        hello world
+        <button>hi</button>
+      </div>
     </div>
   );
 }
